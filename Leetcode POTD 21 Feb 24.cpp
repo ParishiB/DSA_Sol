3bda716a2d71994a8a2467e1d8af6e74185e1163
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-02-21 11:47:11 +0530

    Update Leetcode POTD 21 Feb 24.cpp

--- Leetcode POTD 21 Feb 24.cpp
+++ Leetcode POTD 21 Feb 24.cpp
@@ -1,3 +1,5 @@
+// Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
