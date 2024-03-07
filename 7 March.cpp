ad3f2f9864b9c802744ae746efc615b2d71cd008
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-03-07 11:29:53 +0530

    Create 7 March.cpp

---
+++ 7 March.cpp
@@ -0,0 +1,20 @@
+
+876. Middle of the Linked List
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+
+        if(head == nullptr)return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+        
+    }
+};
